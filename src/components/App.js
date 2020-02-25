e29242f7f144fde3274672d6db98d6fc06e6ba14
Carlos Castillo <info@carloscastillo.cl>
2020-02-25 20:32:50 +0000

    fetch Product list and display

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import axios from 'axios';
+import * as Constant from '../Constants';
+
 import Product from './Product';
 import Header from './Header';
 import Filter from './Filters';
@@ -15,27 +18,42 @@ class App extends React.Component {
             filters: {
                 filterSelectedColour: '',
                 filterColours: '',
-            }
+            },
+            loadingProducts: true
         }
     }
 
     componentDidMount() {
-
+        axios.get(Constant.PRODUCTS_URL)
+            .then(response => {
+                const products = response.data;
+                this.setState({
+                    loadingProducts: false,
+                    products: products
+                });
+            })
     }
 
     render() {
+        const isLoading = this.state.loadingProducts;
+        const products = this.state.products;
         return (
             <div className="App">
                 <Header/>
                 <Filter/>
                 <div className="productContainer container">
-                    <Product/>
-                    <Product/>
-                    <Product/>
-                    <Product/>
-                    <Product/>
-                    <Product/>
-                    <Product/>
+                    {isLoading &&
+                    <div className="p-5 text-center">
+                        <i className="fas fa-spinner fa-spin fa-5x"></i>
+                    </div>
+                    }
+                    {
+                        products.map((product) => {
+                            return (
+                                <Product key={'product-'+product.id} product={product}/>
+                            )
+                        })
+                    }
                 </div>
                 <Basket/>
             </div>
--- src/components/Product.js
+++ src/components/Product.js
@@ -3,18 +3,19 @@ import '../styles/App.scss';
 
 class Product extends React.Component {
     render() {
+        const product = this.props.product;
         return (
             <React.Fragment>
                 <div className="card mb-4">
                     <div className="row no-gutters">
                         <div className="col-md-4">
-                            <img src="https://via.placeholder.com/600x600" className="card-img" alt="..."/>
+                            <img src={product.img} className="card-img" alt="{product.name}"/>
                         </div>
                         <div className="col-md-8">
                             <div className="card-body">
-                                <h2 className="h4 card-title">Product name</h2>
-                                <p className="text-muted small">Colour</p>
-                                <p className="price">$100</p>
+                                <h2 className="h4 card-title">{product.name}</h2>
+                                <p className="text-muted small">{product.colour}</p>
+                                <p className="price">{product.price}</p>
                                 <div className="qtyContainer mb-3 text-center">
                                     <div className="input-group">
                                         <div className="input-group-prepend">
